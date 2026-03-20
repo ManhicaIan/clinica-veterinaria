c4ddc16d5fd7f5878014df2fbbecbe0d6f5e590a
Ian Manhica <ianmanhica4@gmail.com>
2026-03-20 07:18:45 +0200

    add User entity

---
+++ src/main/java/com/manhica/clinica_veterinaria/entity/user/User.java
@@ -0,0 +1,31 @@
+package com.manhica.clinica_veterinaria.entity.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name ="user_tb")
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(nullable = false)
+    private boolean status;
+}
