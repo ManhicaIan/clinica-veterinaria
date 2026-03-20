c953ac1460bd51421f4e066a2304a36f1ede77ef
Ian Manhica <ianmanhica4@gmail.com>
2026-03-20 07:38:46 +0200

    add and confing of application-test.porperties

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO user_tb (id, email, password, status) VALUES (random_uuid(), 'admin@clinica.com', 'admin123', true);
+INSERT INTO user_tb (id, email, password, status) VALUES (random_uuid(), 'joao.silva@clinica.com', 'joao123', true);
+INSERT INTO user_tb (id, email, password, status) VALUES (random_uuid(), 'maria.costa@clinica.com', 'maria123', true);
+INSERT INTO user_tb (id, email, password, status) VALUES (random_uuid(), 'pedro.nunes@clinica.com', 'pedro123', false);
+INSERT INTO user_tb (id, email, password, status) VALUES (random_uuid(), 'ana.lima@clinica.com', 'ana123', true);
